b3463200eddf57d81867aba5f9f0814564f02718
Luca Cirio <luca.cirio@leviahub.com>
2026-04-30 23:08:43 +0200

    New cv commit

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   return (
     <div> {/* Footer */}
         <footer className="py-6 bg-gray-800 text-white text-center">
-            <p>&copy; 2025 Luca Cirio. Tutti i diritti riservati.</p>
+            <p>&copy; 6 Luca Cirio. Tutti i diritti riservati.</p>
         </footer>
     </div>
   )
